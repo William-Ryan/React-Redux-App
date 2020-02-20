97cdfe776d0eb65518ba95d4000c660d48b399a4
William Ryan <billy.ryan.rom@gmail.com>
2020-02-20 06:14:00 -0500

    Add Store, Provider, and Thunk to index.js

--- project/src/index.js
+++ project/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
+const store = createStore(applyMiddleware(thunk));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
